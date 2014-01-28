9dcc7de44008c48cc45d4f1d2c40c2e8adbbd081
Cory <coryisbest0728@gmail.com>
2014-01-28 20:45:08 +0800

    Added the function bind for compatibility

--- src/declare.js
+++ src/declare.js
@@ -6,6 +6,29 @@ define(function () {
 		opts = op.toString,
 		xtor = new Function;
 	
+	// function bind for compatibility
+	if (!Function.prototype.bind) {
+		Function.prototype.bind = function (oThis) {
+			if (typeof this !== "function") {
+				// closest thing possible to the ECMAScript 5 internal IsCallable function
+				throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+			}
+			var aArgs = Array.prototype.slice.call(arguments, 1), 
+				fToBind = this, 
+				fNOP = function () {},
+				fBound = function () {
+					return fToBind.apply(this instanceof fNOP && oThis
+										? this
+										: oThis,
+										aArgs.concat(Array.prototype.slice.call(arguments)));
+				};
+		
+			fNOP.prototype = this.prototype;
+			fBound.prototype = new fNOP();
+			return fBound;
+		};
+	}
+	
 	// err function as warning
 	function err (msg, cls) {
 		throw new Error("declare" + (cls ? " " + cls : "") + ": " + msg);
